7fb463d107f85cefc6439d90973ed6b541fda41a
Sooraj Selvan <soorajselvan026@gmail.com>
2026-02-27 20:55:48 +0530

    Day 02 - Prime number in python

---
+++ Python/day-02.py
@@ -0,0 +1,18 @@
+# Prime Number
+
+while True:
+    num = (input("Enter the number or type 'e' to exit: "))
+    if( num.lower() == 'e'):
+        print("Exiting.....\nVisit again")
+        break
+    else:
+        num = int(num)
+        if(num <= 1):
+            print("1 is not a prime number")
+        else:
+            for i in range(2, num-1):
+                if(num % i == 0):
+                    print("Number is not prime")
+                    break
+            else:
+                print("Number is prime")
