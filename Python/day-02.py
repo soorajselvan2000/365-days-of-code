2ab0cde2eddda76e97bd667f312204e90d95ca0d
Sooraj Selvan <soorajselvan026@gmail.com>
2026-02-27 21:10:57 +0530

    Day 02 - Factorial of a number in Python

--- Python/day-02.py
+++ Python/day-02.py
@@ -1,18 +1,34 @@
-# Prime Number
+# # Prime Number
 
+# while True:
+#     num = (input("Enter the number or type 'e' to exit: "))
+#     if( num.lower() == 'e'):
+#         print("Exiting.....\nVisit again")
+#         break
+#     else:
+#         num = int(num)
+#         if(num <= 1):
+#             print("1 is not a prime number")
+#         else:
+#             for i in range(2, num-1):
+#                 if(num % i == 0):
+#                     print("Number is not prime")
+#                     break
+#             else:
+#                 print("Number is prime")
+
+
+
+
+# Factorial of a number
 while True:
-    num = (input("Enter the number or type 'e' to exit: "))
-    if( num.lower() == 'e'):
-        print("Exiting.....\nVisit again")
+    num = input("Enter the number or type 'e' to exit: ")
+    if(num == 'e'):
+        print("Exiting...\nVisit again.")
         break
-    else:
-        num = int(num)
-        if(num <= 1):
-            print("1 is not a prime number")
-        else:
-            for i in range(2, num-1):
-                if(num % i == 0):
-                    print("Number is not prime")
-                    break
-            else:
-                print("Number is prime")
+    num = int(num)
+    result = 0
+
+    for i in range(1, num+1):
+        result = result + i
+    print(result)
